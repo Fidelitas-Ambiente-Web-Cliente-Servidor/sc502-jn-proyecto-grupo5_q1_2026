d1dcf8a1fb4f7b92f84f330ff56bd2bd782a700c
ERICK BARRIENTOS MONTES <eriickbarriientos42@gmail.com>
2026-04-09 00:01:45 -0600

    Se crea el includes para el header vertical para utilizarlo en admin / vendedor

---
+++ views/includes/components/header_vertical.php
@@ -0,0 +1,14 @@
+<header class="header header--vertical">
+    <h1 class="header__title">ONLY WAY</h1>
+    <nav class="navbar">
+        <ul class="navbar__items">
+            <li class="navbar__item navbar__item--active"><a class="navbar__link" href="#"><i class="bi bi-columns-gap"></i>Dashboard</a></li>
+            <li class="navbar__item"><a class="navbar__link" href="/admin?page=gestion/usuarios"><i class="bi bi-people navbar__icon"></i>Gestión de Usuarios</a></li>
+            <li class="navbar__item"><a class="navbar__link" href="/admin?page=gestion/inventario"><i class="bi bi-box-seam navbar__icon"></i>Gestión de Inventarios</a></li>
+            <li class="navbar__item"><a class="navbar__link" href="/admin?page=gestion/pedidos"><i class="bi bi-cart navbar__icon"></i>Gestión de Pedidos</a></li>
+            <li class="navbar__item"><a class="navbar__link" href="/admin?page=gestion/categorias"><i class="bi bi-diagram-2 navbar__icon"></i>Gestión de Categorías</a></li>
+            <li class="navbar__item"><a class="navbar__link" href="/admin?page=gestion/pagos"><i class="bi bi-credit-card navbar__icon"></i>Gestión de Pagos</a></li>
+            <li class="navbar__item"><a class="navbar__link" href="<?php echo BASE_URL; ?>/?page=logout"><i class="bi bi-box-arrow-right navbar__icon"></i>Salir</a></li>
+        </ul>
+    </nav>
+</header>
