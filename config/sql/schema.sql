09aff5c8bef58cd4231f633f723d5e260d385c32
ERICK BARRIENTOS MONTES <eriickbarriientos42@gmail.com>
2026-04-18 13:17:35 -0600

    Se crean las tablas categorias,productos, tallas, colores, variantes para la version 1.1

---
+++ config/sql/schema.sql
@@ -0,0 +1,41 @@
+CREATE TABLE CATEGORIAS (
+	id_categoria int primary key auto_increment,
+	nombre varchar(100),
+    descripcion varchar(255),
+    estado boolean default true
+);
+
+CREATE TABLE PRODUCTOS (
+	id_producto int primary key auto_increment,
+    id_categoria int,
+    nombre_producto varchar(150),
+    descripcion varchar(255),
+    url_imagen varchar(255),
+    precio_unitario decimal(10,2),
+    estado boolean default true,
+    FOREIGN KEY (id_categoria) REFERENCES CATEGORIAS(id_Categoria) ON DELETE SET NULL
+);
+
+CREATE TABLE COLORES(
+	id_color int primary key auto_increment,
+    nombre_color varchar(150)
+);
+
+CREATE TABLE TALLAS(
+	id_talla int primary key auto_increment,
+    nombre varchar(10)
+);
+
+CREATE TABLE VARIANTES(
+	id_variante int primary key auto_increment,
+    id_producto int,
+    id_color int,
+    id_talla int,
+    stock int default 0,
+    FOREIGN KEY (id_producto) REFERENCES PRODUCTOS(id_producto) ON DELETE CASCADE,
+    FOREIGN KEY (id_color) REFERENCES COLORES(id_color) ON DELETE CASCADE,
+    FOREIGN KEY (id_talla) REFERENCES TALLAS(id_talla) ON DELETE CASCADE
+)
+
+
+
